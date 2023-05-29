875192f64ba9015f8b2ebdabe5e8bf13f95243a5
yussaaa <yusa.li@mail.utoronto.ca>
2023-05-29 15:05:38 -0400

    Add listener to the input and button

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ImageLinkForm.css";
 
-const ImageLinkform = () => {
+const ImageLinkform = ({ onInputChange, onSubmittButtonClick }) => {
   return (
     <div>
       <p className="f3">
@@ -11,9 +11,16 @@ const ImageLinkform = () => {
       </p>
       <div className="center">
         <div className="form center pa4 br3 shadow-5">
-          <input className="f4 pa2 w-70 center" type="tex" />
+          <input
+            className="f4 pa2 w-70 center"
+            type="tex"
+            onChange={onInputChange}
+          />
 
-          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+          <button
+            className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+            onClick={onSubmittButtonClick}
+          >
             Detect
           </button>
         </div>
