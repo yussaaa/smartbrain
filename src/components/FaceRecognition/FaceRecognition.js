92ac9e33a20766ede244bc5e7804369e844f3afe
yussaaa <yusa.li@mail.utoronto.ca>
2023-05-29 15:05:14 -0400

    Add image component

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FaceRecognition = ({ img_url }) => {
+  return (
+    <div>
+      <img src={img_url} />
+    </div>
+  );
+};
+
+export default FaceRecognition;
