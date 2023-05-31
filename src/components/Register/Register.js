c42be6c1c2a855b87c61a4ecd7d1f41e625a2561
yussaaa <yusa.li@mail.utoronto.ca>
2023-05-30 21:23:29 -0400

    optimize css tags

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -4,11 +4,11 @@ const Register = ({ onRouteChange }) => {
   return (
     <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
-        <form className="measure">
+        <div className="measure">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f2 fw6 ph0 mh0">Sign In</legend>
             <div className="mt3">
-              <label className="db fw6 lh-copy f6" for="name">
+              <label className="db fw6 lh-copy f6" htmlFor="name">
                 Name
               </label>
               <input
@@ -19,7 +19,7 @@ const Register = ({ onRouteChange }) => {
               />
             </div>
             <div className="mt3">
-              <label className="db fw6 lh-copy f6" for="email-address">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">
                 Email
               </label>
               <input
@@ -30,7 +30,7 @@ const Register = ({ onRouteChange }) => {
               />
             </div>
             <div className="mv3">
-              <label className="db fw6 lh-copy f6" for="password">
+              <label className="db fw6 lh-copy f6" htmlFor="password">
                 Password
               </label>
               <input
@@ -49,7 +49,7 @@ const Register = ({ onRouteChange }) => {
               value="Register"
             />
           </div>
-        </form>
+        </div>
       </main>
     </article>
   );
