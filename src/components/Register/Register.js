72b190e4cd0e9fa12d081e72f96b576cea3598e4
yussaaa <yusa.li@mail.utoronto.ca>
2023-06-01 10:58:32 -0400

    Update register page displayed text

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -5,8 +5,8 @@ const Register = ({ onRouteChange }) => {
     <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
         <div className="measure">
-          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-            <legend className="f2 fw6 ph0 mh0">Sign In</legend>
+          <fieldset id="register" className="ba b--transparent ph0 mh0">
+            <legend className="f2 fw6 ph0 mh0">Register</legend>
             <div className="mt3">
               <label className="db fw6 lh-copy f6" htmlFor="name">
                 Name
