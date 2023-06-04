47aadfe89c21116098c14c1c95ef72a34ac9b3a8
yussaaa <yusa.li@mail.utoronto.ca>
2023-06-03 23:05:34 -0400

    Not displaying the image if it's in initial state

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -2,22 +2,24 @@ import React from "react";
 import "./FaceRecognition.css";
 
 const FaceRecognition = ({ box, img_url }) => {
-  return (
-    <div className="center ma">
-      <div className="relative mt2">
-        <img id="inputimage" src={img_url} width="500px" height="auto" />
-        <div
-          className="bounding-box"
-          style={{
-            top: box.topRow,
-            right: box.rightCol,
-            bottom: box.bottomRow,
-            left: box.leftCol,
-          }}
-        ></div>
+  if (img_url) {
+    return (
+      <div className="center ma">
+        <div className="relative mt2">
+          <img id="inputimage" src={img_url} width="500px" height="auto" />
+          <div
+            className="bounding-box"
+            style={{
+              top: box.topRow,
+              right: box.rightCol,
+              bottom: box.bottomRow,
+              left: box.leftCol,
+            }}
+          ></div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default FaceRecognition;
